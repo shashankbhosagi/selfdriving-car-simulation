baf54c7ffc849807e72a7f658298630056cc4701
shashankbhosagi <shashankbhosagi0121@gmail.com>
2023-12-30 23:22:45 +0530

    Road definition done

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ canvas.width = 200;
 // COntect to handel everything with car
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(5), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
@@ -14,8 +14,12 @@ function animate() {
   car.update();
   canvas.height = window.innerHeight;
 
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.75);
+
   road.draw(ctx);
   car.draw(ctx);
+  ctx.restore();
   //   Calls animate method again and again which gives illusion of movement
   requestAnimationFrame(animate);
 }
