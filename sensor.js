15ba9f32fcc79cd59798e0ac9341e79bf5b0b003
shashankbhosagi <shashankbhosagi0121@gmail.com>
2023-12-31 01:57:46 +0530

    Sensor almost complete

--- sensor.js
+++ sensor.js
@@ -2,10 +2,11 @@ class Sensor {
   constructor(car) {
     this.car = car;
     this.rayCount = 4;
-    this.rayLength = 100;
+    this.rayLength = 150;
     this.raySpread = Math.PI / 2;
 
     this.rays = [];
+    this.readings = []; //contain distance of borders from rays
   }
 
   #castRays() {
@@ -27,16 +28,54 @@ class Sensor {
     }
   }
 
-  update() {
+  update(roadBorders) {
     this.#castRays();
+    this.readings = [];
+    for (let i = 0; i < this.rayCount; i++) {
+      this.readings.push(this.#getReading(this.rays[i], roadBorders));
+    }
+  }
+  #getReading(ray, roadBorders) {
+    let touches = [];
+
+    for (let i = 0; i < roadBorders.length; i++) {
+      const touch = getIntersection(
+        ray[0],
+        ray[1],
+        roadBorders[i][0],
+        roadBorders[i][1]
+      );
+      if (touch) {
+        this.touches.push(touch);
+      }
+    }
+
+    if (touches.length == 0) {
+      return null;
+    } else {
+      const offsets = touches.map((e) => e.offset); //what this does is, return an array containing all the offset values ie. distance of intersrction from the center of car.
+      const minOffset = Math.min(...offsets); //... it is called spread operator, means destructuring the array in to elements :)
+      return touches.find((e) => e.offset == minOffset);
+    }
   }
   draw(ctx) {
     for (let i = 0; i < this.rayCount; i++) {
+      let end = this.rays[i][1];
+      if (this.readings[i]) {
+        end = this.readings[i];
+      }
       ctx.beginPath();
       ctx.lineWidth = 2;
       ctx.strokeStyle = "yellow";
       ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
-      ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.lineTo(end.x, end.y);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "yellow";
+      ctx.moveTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.lineTo(end.x, end.y);
       ctx.stroke();
     }
   }
