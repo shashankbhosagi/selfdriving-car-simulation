b2997422fbeb067ff121fc1a9ffa527f31d03d10
shashankbhosagi <shashankbhosagi0121@gmail.com>
2023-12-30 16:02:15 +0530

    Add Speed and accelration to car

--- car.js
+++ car.js
@@ -5,22 +5,26 @@ class Car {
     this.width = width;
     this.height = height;
 
+    this.speed = 0;
+    this.acceleration = 0.2;
+
     this.controls = new Controls();
   }
 
   update() {
     if (this.controls.forward) {
-      this.y -= 2;
+      this.speed += this.acceleration;
     }
     if (this.controls.reverse) {
-      this.y += 2;
+      this.speed -= this.acceleration;
     }
-    if (this.controls.left) {
-      this.x -= 2;
-    }
-    if (this.controls.right) {
-      this.x += 2;
-    }
+    this.y -= this.speed;
+    // if (this.controls.left) {
+    //   this.x -= 2;
+    // }
+    // if (this.controls.right) {
+    //   this.x += 2;
+    // }
   }
 
   draw(ctx) {
