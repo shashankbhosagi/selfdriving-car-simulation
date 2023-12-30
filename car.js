85d81229994a5dc5fe357229123ca7cf0ff53228
shashankbhosagi <shashankbhosagi0121@gmail.com>
2023-12-30 16:28:52 +0530

    Flip issue solved (While going backwards) and staitonary turn also fixed

--- car.js
+++ car.js
@@ -37,13 +37,16 @@ class Car {
     if (Math.abs(this.speed) < this.friciton) {
       this.speed = 0;
     }
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
+    }
 
-    if (this.controls.left) {
-      this.angle += 0.03;
-    }
-    if (this.controls.right) {
-      this.angle -= 0.03;
-    }
     this.x -= Math.sin(this.angle) * this.speed;
     this.y -= Math.cos(this.angle) * this.speed;
   }
